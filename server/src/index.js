e8af96ee957b71a6e32fb75e04940e64332b28ff
Jason Ransom <jasonlawrenceransom@gmail.com>
2020-11-15 12:46:48 -0500

    Setup datasources and added to ApolloServer config

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
-server.listen().then(() => {
-  console.log(`
-    Server is running!
-    Listening on port 4000
-    Explore at https://studio.apollographql.com/dev
-  `);
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
 
